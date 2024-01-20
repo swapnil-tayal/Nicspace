3e4cb4558d9d9df318fd0051e4f5fe7f2a1eca71
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-01-21 00:05:18 +0530

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,8 @@
 import Home from "./components/Home"
 
 function App() {
-
-  // const dispatch = useDispatch();
-  
-  // useEffect(() => {
-  //   dispatch(
-  //     setLogin({
-  //       user: null,
-  //       token: null,
-  //     }),
-  //     setPost({
-  //       post:[]
-  //     })
-  //   )
-  // }, []);
-
   return (
-    <>
       <Home />
-    </>
   )
 }
 
